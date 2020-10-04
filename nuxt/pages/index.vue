575119da99179f0d0c59a74c64fec6a656b84ea7
Toma1212 <qd30755778@outlook.jp>
2020-10-04 15:41:17 +0900

    first commit

---
+++ nuxt/pages/index.vue
@@ -0,0 +1,56 @@
+<template>
+  <v-layout
+    column
+    justify-center
+    align-center
+  >
+    <v-flex
+      xs12
+      sm8
+      md6
+    >
+      <div class="container">
+        <div>
+          <logo />
+          <h1 class="title">
+            {{data}}
+          </h1>
+          <h2 class="subtitle">
+            My superb Nuxt.js project
+          </h2>
+          <div class="links">
+            <a
+              href="https://nuxtjs.org/"
+              target="_blank"
+              class="button--green"
+            >
+              Documentation
+            </a>
+            <a
+              href="https://github.com/nuxt/nuxt.js"
+              target="_blank"
+              class="button--grey"
+            >
+              GitHub
+            </a>
+          </div>
+        </div>
+      </div>
+    </v-flex>
+  </v-layout>
+</template>
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+  components: {
+    Logo
+  },
+  async asyncData(app) {
+    const data = await app.$axios.$get('/api')
+    return {
+      data
+    }
+  }
+}
+</script>
